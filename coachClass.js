6e80095febc0f7842725deb5a0e87f85a5e4ae20
Alex de Mulder <alexdemulder@gmail.com>
2015-11-24 18:32:28 +0100

    added API suggestions for stubs, bug fixes

--- coachClass.js
+++ coachClass.js
@@ -12,15 +12,15 @@ mealTimeCoach.state = {
 mealTimeCoach.setup = function() {
   // this could be extended to have the FULL specification of the sensors. It would be the requirement definition.
   // binding the callback to the this with the state will be done here
-  notifications.listen("meal_time_breakfast", this.onSensorData);
-  notifications.listen("meal_time_lunch",     this.onSensorData);
-  notifications.listen("meal_time_dinner",    this.onSensorData);
+  Notifications.listen("meal_time_breakfast", this.onSensorData);
+  Notifications.listen("meal_time_lunch",     this.onSensorData);
+  Notifications.listen("meal_time_dinner",    this.onSensorData);
 
   // will fire at the endtime regardless, triggerTimes are other times this will trigger at.
   // binding the callback to the this with the state will be done here
-  coachingEngine.addTask({sensors: ["meal_time_breakfast"], repeat: "daily", startTime: "00:00", endTime: "23:59", triggerTimes: ["08:00"]}, this.evaluate);
-  coachingEngine.addTask({sensors: ["meal_time_lunch"],     repeat: "daily", startTime: "00:00", endTime: "23:59", triggerTimes: ["13:00"]}, this.evaluate);
-  coachingEngine.addTask({sensors: ["meal_time_dinner"],    repeat: "daily", startTime: "00:00", endTime: "23:59", triggerTimes: ["18:30"]}, this.evaluate);
+  CoachingEngine.addTask({sensors: ["meal_time_breakfast"], repeat: "daily", startTime: "00:00", endTime: "23:59", triggerTimes: ["08:00"]}, this.evaluate);
+  CoachingEngine.addTask({sensors: ["meal_time_lunch"],     repeat: "daily", startTime: "00:00", endTime: "23:59", triggerTimes: ["13:00"]}, this.evaluate);
+  CoachingEngine.addTask({sensors: ["meal_time_dinner"],    repeat: "daily", startTime: "00:00", endTime: "23:59", triggerTimes: ["18:30"]}, this.evaluate);
 }
 
 
@@ -31,7 +31,7 @@ mealTimeCoach.setup = function() {
  */
 mealTimeCoach.onSensorData = function(sensor, data) {
   this.state[sensor].value += 1;
-  coachingEngine.updateTaskForSensor(sensor, data);
+  CoachingEngine.updateTaskForSensor(sensor, data);
 }
 
 
@@ -44,30 +44,28 @@ mealTimeCoach.onSensorData = function(sensor, data) {
  */
 mealTimeCoach.evaluate = function(task, time, sensor, data) {
   if (sensor === undefined) {sensor = task.sensors[0];} // here we know that there is only one sensor so we can write it like this.
-  var stateData = this.state[sensor];
-  var value = stateData.value;
-  var targetTime = util.timeToDailyTimestamp(stateData.time, task.originDate);
-  var endTime = util.timeToDailyTimestamp(stateData.time, task.originDate);
-  var offset = 30 * 60 * 1000;
+  var targetTime = util.timeToDailyTimestamp(this.state[sensor].time, task.originDate);
+  var endTime = util.timeToDailyTimestamp(this.state[sensor].time, task.originDate);
+  var offset = 30 * 60 * 1000; // 30 mins in milliseconds
 
-  if (value == 0) {
+  if (this.state[sensor].value == 0) {
     if (time > targetTime + offset) {
-      coachingEngine.send(this.state.id, "reminder");
+      CoachingEngine.send(this.state.id, {message: "reminder"});
     }
 
     if (time > endTime) {
-      coachingEngine.send(this.state.id, "unknown");
-      coachingEngine.finish(scheduleInformation);
+      CoachingEngine.send(this.state.id, {message: "unknown"});
+      CoachingEngine.finish(task);
     }
   }
   else {
     if (time <= targetTime + offset || time >= targetTime - offset) {
-      coachingEngine.send(this.state.id, "success")
-      coachingEngine.finish(scheduleInformation);
+      CoachingEngine.send(this.state.id, {message: "success"})
+      CoachingEngine.finish(task);
     }
     else {
-      coachingEngine.send(this.state.id, "failure")
-      coachingEngine.finish(scheduleInformation);
+      CoachingEngine.send(this.state.id, {message: "failure"})
+      CoachingEngine.finish(task);
     }
   }
 }
@@ -80,23 +78,76 @@ mealTimeCoach.evaluate = function(task, time, sensor, data) {
 // ********************** stubs for required support classes: ********************** //
 
 // used to handle tasks. Tasks are automatically rescheduled based on their times and the finish method.
-var coachingEngine = {};
-coachingEngine.state               = {}; // state will be persisted
-coachingEngine.addTask             = function(options, callback) {};
-coachingEngine.updateTaskForSensor = function(sensor, data) {};
-coachingEngine.finish              = function(information) {};
+var CoachingEngine = {};
+CoachingEngine.state = {}; // state will be persisted
+/**
+ * API suggestion:
+ * CoachingEngine will handle tasks. A task is a maintained scheduled entity.
+ * The task will always ALSO trigger on the endTime. At this point it is expected that finish will be called.
+ *
+ * @param {Object}   options    |  {
+ *                              |    sensors:   Array    // array of sensors that will be triggering the callback,
+ *                              |    repeat:    String,  // "daily"/"weekly" which defines how often the task is repeated
+ *                              |    startTime: String,  // "00:00" to indicate when the task starts. In case of weekly it will be for the first day.
+ *                              |    endTime:   String,  // "23:59" to indicate when the task ends. In case of weekly it will be for the last day.
+ *                              |    startDay:  String,  // day of the week, Monday, Tuesday etc. Only relevant for weekly. Default: Monday
+ *                              |    endDay:    String,  // day of the week, Monday, Tuesday etc. Only relevant for weekly. Default: Sunday
+ *                              |    triggers:  Object   // {days: [Monday, Tuesday, ..], times: ["00:24","8:30", ..]} Object that defines when triggers will be fired.
+ *                              |  }
+ * @param {Function} callback
+ */
+CoachingEngine.addTask = function(options, callback) {};
 
-// used to send and listen to notifications.
-var notifications = {};
-notifications.send   = function() {};
-notifications.listen = function(topic, callback) {};
+/**
+ * Will fire a task with the callback if there exists a task with this sensor in the array of relevant sensors.
+ * @param {String} sensor
+ * @param {Object} data
+ */
+CoachingEngine.updateTaskForSensor = function(sensor, data) {};
 
-// the scheduler will be used by the coaching engine entity
-var scheduler = {};
-scheduler.state     = {}; // this will store the scheduled things and will be persisted.
-scheduler.schedule  = function(options, callback) {};
-scheduler.reset     = function() {}; // resets all scheduled callbacks
+/**
+ * This should mark the task as finished, making sure it will ONLY start listening for new date when the new period has been started
+ * This is the what makes the task system better than the normal usage of the Scheduler.
+ * @param information
+ */
+CoachingEngine.finish = function(information) {};
+
+
+
+// used to send and listen to Notifications.
+var Notifications = {};
+/**
+ * This will notify the phone app with a certain topic and data
+ * @param {String} topic
+ * @param {Object} data
+ */
+Notifications.send   = function(topic, data) {};
+Notifications.listen = function(topic, callback) {};
+
+
+
+// the Scheduler will be used by the coaching engine entity
+var Scheduler = {};
+Scheduler.state = {}; // this will store the scheduled things and will be persisted.
+/**
+ * API suggestion:
+ * Scheduler would be able to schedule firing of a certain function after a certain time.
+ * It would also be able to be triggered manually and removed
+ *
+ * @param {Object} options     |   {
+ *                             |     fromNow: Number, // Number of seconds from current time.
+ *                             |     atTime:  Number, // TimeStamp at which the callback will be executed.
+ *                             |     daily:   String, // A time definition "08:30" which will fire every day
+ *                             |     weekly:  Object, // {day:[Monday, ...], time: "08:30"} define a weekday as String and a time that day as String
+ *                             |   }
+ * @param {Function} callback
+ * @return {String} UUID
+ */
+Scheduler.schedule  = function(options, callback) {}; // schedule a callback
+Scheduler.remove    = function(uuid) {};              // remove callback with UUID
+Scheduler.reset     = function() {};                  // resets all scheduled callbacks
 
 // an object with util methods. Stateless.
 var util = {};
 util.timeToDailyTimestamp = function (timeString, originDateTimeStamp) {}; // convert a string time "08:30" to a timestamp in the day of the originDate
+util.uuid                 = function () {};                                // generate an uuid-v4
